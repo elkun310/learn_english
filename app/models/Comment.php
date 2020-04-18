250d077dde1486276dfe1d6a92ce6f04304ea39f
elkun310 <nguyenha9772@ymail.com>
2020-04-18 21:44:53 +0700

    tạo model

---
+++ app/models/Comment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = "comment";
+
+}
---
+++ app/models/User_Question.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User_Question extends Model
+{
+    protected $table = "user_question";
+
+}
