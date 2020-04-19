3b125e0d240f9e2ae6680f78fbb20d9802e9fbb0
elkun310 <nguyenha9772@ymail.com>
2020-04-19 12:30:18 +0700

    update câu lệnh sửa bảng users

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,15 @@ use App\User;
 Route::get('/', function () {
     return view('welcome');
 });
+// alter table users
+Route::get('alter_user_table', function () {
+    Schema::table('users', function ($table) {
+        $table->string('phone',11)->nullable()->change();
+        $table->integer('level')->nullable()->default('1')->change();
+    });
+    echo "ok";
+});
+
 // Route::get('add_user', function () {
 //     $user = new User;
 //     $user->firstname = "elkun";
@@ -23,6 +32,7 @@ Route::get('/', function () {
 //     $user->email = "elkun310@gmail.com";
 //     $user->password = bcrypt('123456');
 //     $user->phone = "0389938822";
+//     $user->level = "1";
 //     $user->role = "1";
 //     $user->save();
 // });
