b85d874e88cb9aceaf717b01bac3eea704ada823
elkun310 <nguyenha9772@ymail.com>
2020-05-03 15:23:18 +0700

    custom css

--- resources/views/backend/staff/list_staff.blade.php
+++ resources/views/backend/staff/list_staff.blade.php
@@ -12,92 +12,90 @@
     </section>
     <hr>
     <section class="content">
-            <div align="right" style="margin: 20px 0;">
-                <button class="btn btn-success" id="btnAdd">Add</button>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-bordered table-striped" id="staff_table">
-                    <thead>
-                        <tr>
-                            <td>Họ tên</td>
-                            <td>Địa chỉ email</td>
-                            <td>Số điện thoại</td>
-                            <td>Vai trò</td>
-                            <th>Thao tác</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-
-            <div id="formModal" class="modal fade" role="dialog">
-                <div class="modal-dialog">
-                 <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Thêm nhân viên</h4>
-                    </div>
-                    <div class="modal-body">
-                        <span id="form_result"></span>
-                        <form method="post" id="staff_form" class="form-horizontal">
-                            @csrf
-                            <div class="form-group">
-                                <label class="control-label col-md-4" >Tên : </label>
-                                <div class="col-md-8">
-                                    <input type="text" name="firstname" id="firstname" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Họ : </label>
-                                <div class="col-md-8">
-                                    <input type="text" name="lastname" id="lastname" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Email : </label>
-                                <div class="col-md-8">
-                                    <input type="email" name="email" id="email" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Số điện thoại : </label>
-                                <div class="col-md-8">
-                                    <input type="text" name="phone" id="phone" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Mật khẩu : </label>
-                                <div class="col-md-8">
-                                    <input type="password" name="password" id="password" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Nhập lại mật khẩu : </label>
-                                <div class="col-md-8">
-                                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Quyền:</label>
-                                <div class="col-md-8">
-                                    <select id="role" class="form-control" name="role">
-                                        <option value="1">Quản trị</option>
-                                        <option value="0">Nhân viên</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <br />
-                            <div class="form-group" align="center">
-                                <input type="hidden" name="action" id="action" />
-                                <input type="hidden" name="hidden_id" id="hidden_id" />
-                                <input type="submit" name="action_button" id="action_button" class="btn btn-warning" value="Thêm" />
-                            </div>
-                        </form>
-                       </div>
-                    </div>
-                </div>
-            </div>
-
+        <div align="right" style="margin: 20px 0;">
+            <button class="btn btn-success" id="btnAdd">Add</button>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped" id="staff_table">
+                <thead>
+                    <tr>
+                        <td>Họ tên</td>
+                        <td>Địa chỉ email</td>
+                        <td>Số điện thoại</td>
+                        <td>Vai trò</td>
+                        <th>Thao tác</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </section>
+    <div id="formModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+         <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Thêm nhân viên</h4>
+            </div>
+            <div class="modal-body">
+                <span id="form_result"></span>
+                <form method="post" id="staff_form" class="form-horizontal">
+                    @csrf
+                    <div class="form-group">
+                        <label class="control-label col-md-4" >Tên : </label>
+                        <div class="col-md-8">
+                            <input type="text" name="firstname" id="firstname" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-4">Họ : </label>
+                        <div class="col-md-8">
+                            <input type="text" name="lastname" id="lastname" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-4">Email : </label>
+                        <div class="col-md-8">
+                            <input type="email" name="email" id="email" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-4">Số điện thoại : </label>
+                        <div class="col-md-8">
+                            <input type="text" name="phone" id="phone" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-4">Mật khẩu : </label>
+                        <div class="col-md-8">
+                            <input type="password" name="password" id="password" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-4">Nhập lại mật khẩu : </label>
+                        <div class="col-md-8">
+                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-4">Quyền:</label>
+                        <div class="col-md-8">
+                            <select id="role" class="form-control" name="role">
+                                <option value="1">Quản trị</option>
+                                <option value="0">Nhân viên</option>
+                            </select>
+                        </div>
+                    </div>
+                    <br />
+                    <div class="form-group" align="center">
+                        <input type="hidden" name="action" id="action" />
+                        <input type="hidden" name="hidden_id" id="hidden_id" />
+                        <input type="submit" name="action_button" id="action_button" class="btn btn-warning" value="Thêm" />
+                    </div>
+                </form>
+               </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('script')
     @parent
